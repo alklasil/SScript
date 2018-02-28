857852d714f4c0ac594d75b29ceb3b79bff78307
alklasil <alklasil@student.oulu.fi>
2018-02-28 19:39:12 +0200

    update Expression.cpp to match SScriptCompiler

--- src/Expression.cpp
+++ src/Expression.cpp
@@ -29,17 +29,11 @@ int32_t Expression::execute() {
     int32_t *rightValue;
     int32_t *functionIndex;
 
-    // There are always at least 2 elements: leftValue rightValue, often function, default function is *leftValue = *rightValue
+    // There are always at least 1 elements:
     // leftValue must alywas be of mode pointer, it does not care about accessMode (-> somewhat restricted self modification of scripts, but faster)
-    leftValue = &elements[0];
-    leftValue = &sScript.variables[*leftValue];
-
-    if (elementCount == 2) { // "0 1" -> variables[0] = variables[1] or 1, TODO: perhaps combine this and the branch below into one -> clearer
-        rightValue = &elements[1];
-        if (sScript.accessMode == sScript.accessModePointer) {
-            rightValue = &sScript.variables[*rightValue];
-        }
-    } else {
+    if (elementCount >= 3) { // "0 1" -> variables[0] = variables[1] or 1, TODO: perhaps combine this and the branch below into one -> clearer
+        leftValue = &elements[0];
+        leftValue = &sScript.variables[*leftValue];
         // There can be more elements though
         for (int32_t i = 1; i < elementCount; i += 2) {
 
@@ -60,12 +54,26 @@ int32_t Expression::execute() {
 
             if (sScript.abortExpressionExecution != 0) {
 
-                return -1;
+                return 1;
 
             }
 
         }
-    }
+    } else {
+      if (elementCount == 1) {
+         // if elementCount == 1: always only a function
+         leftValue = rightValue = functionIndex = &elements[0];
+         _functions[*functionIndex](leftValue, rightValue);
+         // there is nothing to set, simply return
+         return 2;
+      } else if (elementCount == 2) {
+         // if elementCount == 2: variable[elements[0]] = elements[1]
+         //  (variable = constant), do not care about access mode
+         leftValue = &elements[0];
+         leftValue = &sScript.variables[*leftValue];
+         rightValue = &elements[1];
+      }
+   }
 
     *leftValue = *rightValue;
     return 0;
