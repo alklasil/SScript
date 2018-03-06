c62c79e908c2306e55c15a04da3b0a49b986077e
alklasil <alklasil@student.oulu.fi>
2018-03-06 03:08:54 +0200

    update doc

--- test/Readme.md
+++ test/Readme.md
@@ -3,8 +3,8 @@
 * [ ] SScript::set(buffer) works (i.e., configuration works and tested)
 
 
-# Link, compile, run
+# Link, compile, run (modify the paths as to match yours)
 ´´´
-$ g++ *.cpp ../src/*.cpp -std=c++11 -I ../src -o main
+$ g++ -g *.cpp ../src/*.cpp ../../../ESP/ESP/ESP/teensySoftware/teensySoftware/ESP*.cpp -std=c++11 -I ../src -I ../../../ESP/ESP/ESP/teensySoftware/teensySoftware -o main main
 $ ./main
 ´´´
--- test/main.cpp
+++ test/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include "SScript.h"
+// ESPConfiguration can be changed to another
+// The configuration file must provide extern
+// functions, nothing else.
+// see https://github.com/alklasil/ESP/blob/master/teensySoftware/teensySoftware/ESPConfiguration.cpp
+// for more detail
 #include "ESPConfiguration.h"
 
-// example configuration:
+// example configuration (not working anymore, too old):
 // ./main "9 4 2 15 3 10 5 1 6 1 4 1 3 6 4 10 1 6 6 4 9 2 4 8 6 3 8 5 13 3 0 5 23 5 7 3 4 0 5 8 6 7 11 4 9 3 4 8 3 1 5 0 3 1 4 24 4 3 8 5 13 3 0 5 23 5 7 2 4 0 6 8 6 7 11 4 9 2 4 8"
 
 int main(int argc, char* argv[])
 {
-  // not tested as of yet
 
   if (argc < 2) {
      printf("Usage: %s %s \n", argv[0], "<CONFIGURATION>");
